0ebf0367f0f898986978f70688d32b745b5fd86b
Archit-04 <architar@sheridancollege.ca>
2024-09-20 08:44:49 -0400

    Update NewProject.java

--- src/newproject/NewProject.java
+++ src/newproject/NewProject.java
@@ -14,6 +14,7 @@ public class NewProject {
      * @param args the command line arguments
      */
     // First change
+    //Second change
     public static void main(String[] args) {
         // TODO code application logic here
     }
